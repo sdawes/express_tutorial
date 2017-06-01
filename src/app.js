6d2a57e8bae6bdc23e5326570b8df3e900619925
Stephen Dawes <sdawes@outlook.com>
2017-06-01 23:17:24 +0100

    continue on tutorial with templates, partials and DRY code

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
 'use strict';
 
-var express = require('express');
-var posts = require('./mock/posts.json');
+var express = require('express'),
+	  posts = require('./mock/posts.json');
 
 var app = express();
 
+app.use('/static', express.static(__dirname + '/public'));
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
 app.get('/', function(req, res){
-	res.send('<h1>I am loving Treehouse!</h1>');
-})
+	res.render('index');
+});
 
-app.get('/blog/:title?', function(req, res){
-
+app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.send('This page is under construction');
+		res.send("This page is under construction!");
 	} else {
-		var post = posts[title];
-		res.send(post);
+		var post = posts[title] || {};
+		res.render('post', { post: post});
 	}
-	
-})
+});
 
-
-
-app.listen(3000, function(){
-	console.log('The frontend server is running on port 3000!');
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
 });
